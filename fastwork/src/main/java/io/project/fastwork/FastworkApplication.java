2abba7a48a874dd2a8ff6f6e52f099f432733712
Tim01Bro <arthaos14@gmail.com>
2023-02-03 23:18:52 +0300

    Init commit

---
+++ fastwork/src/main/java/io/project/fastwork/FastworkApplication.java
@@ -0,0 +1,13 @@
+package io.project.fastwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastworkApplication.class, args);
+	}
+
+}
---
+++ fastwork/src/test/java/io/project/fastwork/FastworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.fastwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
