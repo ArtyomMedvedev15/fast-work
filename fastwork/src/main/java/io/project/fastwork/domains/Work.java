f32ba1d80415c9cfcd744f8fa64cdf10b7ac40e5
Tim01Bro <arthaos14@gmail.com>
2023-02-03 23:58:10 +0300

    Work

--- fastwork/src/main/java/io/project/fastwork/domains/Work.java
+++ fastwork/src/main/java/io/project/fastwork/domains/Work.java
@@ -1,4 +1,24 @@
 package io.project.fastwork.domains;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Data
+@EqualsAndHashCode
+@ToString
+@Builder
 public class Work {
+    private Long id;
+    private String workName;
+    private String workDescribe;
+    private Integer workCountPerson;
+    private Float workPrice;
+    private Location workLocation;
+    private Timestamp workDateCreate;
+    private StatusWork workStatus;
+    private User workHirer;
 }
