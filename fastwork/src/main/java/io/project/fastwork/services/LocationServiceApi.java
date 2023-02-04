89b8b47e470f633a251ea4e2c4f2db4fe9b72150
Tim01Bro <arthaos14@gmail.com>
2023-02-04 14:59:18 +0300

    Location service api

--- fastwork/src/main/java/io/project/fastwork/services/LocationServiceApi.java
+++ fastwork/src/main/java/io/project/fastwork/services/LocationServiceApi.java
@@ -1,8 +1,19 @@
 package io.project.fastwork.services;
 
+import io.project.fastwork.domains.Location;
+import io.project.fastwork.domains.Work;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface LocationServiceApi {
+    Location saveLocation(Location savedLocation);
+    Location updateLocation(Location updatedLocation);
+    Location deleteLocation(Location deletedLocation);
+    Location getByWork(Work work);
+    List<Location>findLocationByCity(String city);
+    List<Location>findLocationByNearby(double x,double y);
 
 }
