dfed232433981fc051b219890c06d52fc0233f5a
Tim01Bro <arthaos14@gmail.com>
2023-02-04 14:21:23 +0300

    Notification repository

---
+++ fastwork/src/main/java/io/project/fastwork/repositories/NotificationRepository.java
@@ -0,0 +1,9 @@
+package io.project.fastwork.repositories;
+
+import io.project.fastwork.domains.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+}
