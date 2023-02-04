c2f8a63cc3699cbd6539d06d9be09a969667035c
Tim01Bro <arthaos14@gmail.com>
2023-02-04 14:01:26 +0300

    User repository

--- fastwork/src/main/java/io/project/fastwork/repositories/UserRepository.java
+++ fastwork/src/main/java/io/project/fastwork/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package io.project.fastwork.repositories;
 
+import io.project.fastwork.domains.StatusUser;
 import io.project.fastwork.domains.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,12 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<Users,Long> {
     @Query("select u from Users u where u.userName=:username")
     Users findByUserName(@Param("username")String username);
+
+    @Query("select u from Users u where u.userEmail=:email")
+    Users findbyEmail(@Param("email")String email);
+
+    @Query("update Users u set u.userStatus=:statusUser")
+    Users userBlocked(@Param("statusUser") StatusUser statusUser);
+    @Query("update Users u set u.userStatus=:statusUser")
+    Users userDeleted(@Param("statusUser") StatusUser statusUser);
 }
