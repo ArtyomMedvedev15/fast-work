0f5ee05d4be76173ff805531be5ac0adfe4b0725
Tim01Bro <arthaos14@gmail.com>
2023-02-04 14:02:34 +0300

    Type work repository

---
+++ fastwork/src/main/java/io/project/fastwork/repositories/TypeWorkRepository.java
@@ -0,0 +1,9 @@
+package io.project.fastwork.repositories;
+
+import io.project.fastwork.domains.TypeWork;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TypeWorkRepository extends JpaRepository<TypeWork,Long> {
+}
