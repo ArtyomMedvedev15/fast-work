0cc15faa4e2d4afe1d504391ee5a2e5399f51f28
Tim01Bro <arthaos14@gmail.com>
2023-02-04 13:44:14 +0300

    Location repository

--- fastwork/src/main/java/io/project/fastwork/repositories/LocationRepository.java
+++ fastwork/src/main/java/io/project/fastwork/repositories/LocationRepository.java
@@ -2,8 +2,17 @@ package io.project.fastwork.repositories;
 
 import io.project.fastwork.domains.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location,Long> {
+    @Query("select l from Location l where l.locationPoints.x>=:x and l.locationPoints.y<=y")
+    List<Location>findLocationByNearby(@Param("x")double x, @Param("y")double y);
+    @Query("select l from Location l where l.locationCity=:city")
+    List<Location>findLocationByCity(@Param("city")String city);
+
 }
