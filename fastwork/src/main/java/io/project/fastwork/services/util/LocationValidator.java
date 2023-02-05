02af9145f5b857038c4db6512606fdaebb5596b1
Tim01Bro <arthaos14@gmail.com>
2023-02-05 14:01:07 +0300

    Fix regular expression

--- fastwork/src/main/java/io/project/fastwork/services/util/LocationValidator.java
+++ fastwork/src/main/java/io/project/fastwork/services/util/LocationValidator.java
@@ -9,10 +9,10 @@ public class LocationValidator {
     public static boolean LocationValidDataValues(Location location) throws LocationWithInvalidArguments {
         double map_coordinate_x = Double.parseDouble(location.getLocationPoints().getX().toString());
         double map_coordinate_y = Double.parseDouble(location.getLocationPoints().getY().toString());
-        if ((Pattern.matches("^[a-zA-Z]+$", location.getLocationCity()) &&
-                Pattern.matches("^[a-zA-Z]+$", location.getLocationCountry()) &&
-                Pattern.matches("^[a-zA-Z]+$", location.getLocationRegion()) &&
-                Pattern.matches("^[a-zA-Z]+$", location.getLocationStreet())) && (
+        if ((Pattern.matches("^[a-zA-Z]{4,30}+$", location.getLocationCity()) &&
+                Pattern.matches("^[a-zA-Z]{4,30}+$", location.getLocationCountry()) &&
+                Pattern.matches("^[a-zA-Z]{4,45}+$", location.getLocationRegion()) &&
+                Pattern.matches("^[a-zA-Z]{3,45}+$", location.getLocationStreet())) && (
                 (map_coordinate_x >= 0 && map_coordinate_x <= 180) && (map_coordinate_y >= 0 && map_coordinate_y <= 90)
         )){
            return true;
