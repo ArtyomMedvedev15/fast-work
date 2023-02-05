9fef57eaf6e6644f3e46adedf166abdf3c72b52c
Tim01Bro <arthaos14@gmail.com>
2023-02-05 13:08:18 +0300

    Work application service

---
+++ fastwork/src/main/java/io/project/fastwork/services/WorkApplicationService.java
@@ -0,0 +1,15 @@
+package io.project.fastwork.services;
+
+import io.project.fastwork.domains.Work;
+import io.project.fastwork.domains.WorkApplication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface WorkApplicationService {
+    WorkApplication saveWorkApplication(WorkApplication savedWorkApplication);
+    WorkApplication rejectedWorkApplication(WorkApplication rejectedWorkApplication);
+    WorkApplication approvedWorkApplication(WorkApplication approvedWorkApplication);
+    List<WorkApplication>findAllWorkApplicationByWork(Work work);
+}
