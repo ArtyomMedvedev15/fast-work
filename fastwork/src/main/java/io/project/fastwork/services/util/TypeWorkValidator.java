6317d09c8c7f8a35964e374e00063d6acd4a05c2
Tim01Bro <arthaos14@gmail.com>
2023-02-06 22:49:32 +0300

    Type work validator

--- fastwork/src/main/java/io/project/fastwork/services/util/TypeWorkValidator.java
+++ fastwork/src/main/java/io/project/fastwork/services/util/TypeWorkValidator.java
@@ -1,4 +1,17 @@
 package io.project.fastwork.services.util;
 
+import io.project.fastwork.domains.TypeWork;
+import io.project.fastwork.services.exception.TypeWorkInvalidParameterException;
+
+import java.util.regex.Pattern;
+
 public class TypeWorkValidator {
+    public static boolean TypeWorkValidator(TypeWork typeWork) throws TypeWorkInvalidParameterException {
+        if(Pattern.matches("^[a-zA-Z]{4,30}+$",typeWork.getTypeWorkName()) &&
+                Pattern.matches("^[a-zA-Z]{4,256}+$",typeWork.getTypeWorkDescribe())){
+           return true;
+        }else{
+            throw new TypeWorkInvalidParameterException("Check string parameters, something was wrong!");
+        }
+    }
 }
