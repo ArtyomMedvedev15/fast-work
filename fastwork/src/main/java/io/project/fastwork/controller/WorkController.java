497789386b8c116bd41954b8efb0c90c59e4a108
Tim01Bro <arthaos14@gmail.com>
2023-03-04 22:35:05 +0300

    Get all work

--- fastwork/src/main/java/io/project/fastwork/controller/WorkController.java
+++ fastwork/src/main/java/io/project/fastwork/controller/WorkController.java
@@ -1,17 +1,28 @@
 package io.project.fastwork.controller;
 
+import io.project.fastwork.domains.Work;
 import io.project.fastwork.services.api.WorkServiceApi;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/work")
+@RequestMapping("/api/v1/work")
 @RequiredArgsConstructor
 @Slf4j
 public class WorkController {
     private final WorkServiceApi workService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?>getAllWorks(){
+        List<Work>workList=workService.findAllOpenedWork();
+        return ResponseEntity.ok().body(workList);
+    }
 
 
 }
