c215eccc5c877ca83ed1a330821de6970d853932
Tim01Bro <arthaos14@gmail.com>
2023-03-05 23:39:23 +0300

    Save type work

--- fastwork/src/main/java/io/project/fastwork/controller/TypeWorkController.java
+++ fastwork/src/main/java/io/project/fastwork/controller/TypeWorkController.java
@@ -1,16 +1,23 @@
 package io.project.fastwork.controller;
 
+import io.project.fastwork.controller.exception.RestTypeWorkAlreadyExistsException;
+import io.project.fastwork.controller.exception.RestTypeWorkInvalidParameterException;
+import io.project.fastwork.domains.TypeWork;
+import io.project.fastwork.dto.request.TypeWorkSaveRequest;
 import io.project.fastwork.dto.response.TypeWorkResponse;
-import io.project.fastwork.dto.util.TypeWorkDtoUtl;
+import io.project.fastwork.dto.util.TypeWorkDtoUtil;
 import io.project.fastwork.services.api.TypeWorkServiceApi;
+import io.project.fastwork.services.exception.TypeWorkAlreadyExistsException;
+import io.project.fastwork.services.exception.TypeWorkInvalidParameterException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static io.project.fastwork.dto.util.TypeWorkDtoUtil.getTypeWorkFromRequest;
+import static io.project.fastwork.dto.util.TypeWorkDtoUtil.getTypeWorkResponse;
 
 @RestController
 @RequestMapping("/api/v1/typework")
@@ -22,8 +29,22 @@ public class TypeWorkController {
 
     @GetMapping("/all")
     public ResponseEntity<?>getAllTypeWork(){
-        List<TypeWorkResponse>typeWorkResponseList = typeWorkService.findAll().stream().map(TypeWorkDtoUtl::getTypeWorkResponse).toList();
+        List<TypeWorkResponse>typeWorkResponseList = typeWorkService.findAll().stream().map(TypeWorkDtoUtil::getTypeWorkResponse).toList();
         return ResponseEntity.ok().body(typeWorkResponseList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?>saveTypeWork(@RequestBody TypeWorkSaveRequest typeWorkSaveRequest){
+        TypeWork typeWorkSave = getTypeWorkFromRequest(typeWorkSaveRequest);
+        try {
+            typeWorkSave = typeWorkService.saveTypeWork(typeWorkSave);
+        } catch (TypeWorkInvalidParameterException e) {
+            throw new RestTypeWorkInvalidParameterException("Type work data isn't correct!");
+        } catch (TypeWorkAlreadyExistsException e) {
+            throw new RestTypeWorkAlreadyExistsException(String.format("Type work with name - %s already exists!",typeWorkSaveRequest.getTypeWorkName()));
+        }
+        TypeWorkResponse typeWorkResponse = getTypeWorkResponse(typeWorkSave);
+        return ResponseEntity.ok().body(typeWorkResponse);
     }
 
 }
--- fastwork/src/main/java/io/project/fastwork/dto/util/TypeWorkDtoUtil.java
+++ fastwork/src/main/java/io/project/fastwork/dto/util/TypeWorkDtoUtil.java
@@ -1,15 +1,23 @@
 package io.project.fastwork.dto.util;
 
 import io.project.fastwork.domains.TypeWork;
+import io.project.fastwork.dto.request.TypeWorkSaveRequest;
 import io.project.fastwork.dto.response.TypeWorkResponse;
 
-public class TypeWorkDtoUtl {
+public class TypeWorkDtoUtil {
 
     public static TypeWorkResponse getTypeWorkResponse(TypeWork typeWork){
         return TypeWorkResponse.builder()
                 .id(typeWork.getId())
                 .typeWorkName(typeWork.getTypeWorkName())
                 .typeWorkDescribe(typeWork.getTypeWorkDescribe())
+                .build();
+    }
+
+    public static TypeWork getTypeWorkFromRequest(TypeWorkSaveRequest typeWorkSaveRequest){
+        return TypeWork.builder()
+                .typeWorkName(typeWorkSaveRequest.getTypeWorkName())
+                .typeWorkDescribe(typeWorkSaveRequest.getTypeWorkDescribe())
                 .build();
     }
 }
