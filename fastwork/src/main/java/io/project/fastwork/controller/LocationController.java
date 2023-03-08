54948e1a76602d41744f2a0d631e50b0dfca39e6
Tim01Bro <arthaos14@gmail.com>
2023-03-08 17:21:00 +0300

    Find location by city

--- fastwork/src/main/java/io/project/fastwork/controller/LocationController.java
+++ fastwork/src/main/java/io/project/fastwork/controller/LocationController.java
@@ -2,16 +2,18 @@ package io.project.fastwork.controller;
 
 import io.project.fastwork.controller.exception.RestLocationNotFoundException;
 import io.project.fastwork.domains.Location;
+import io.project.fastwork.dto.request.LocationByCityRequest;
 import io.project.fastwork.dto.response.LocationResponse;
+import io.project.fastwork.dto.util.LocationDtoUtil;
 import io.project.fastwork.services.api.LocationServiceApi;
 import io.project.fastwork.services.exception.LocationNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.project.fastwork.dto.util.LocationDtoUtil.getLocationResponseFromDomain;
 
@@ -20,9 +22,7 @@ import static io.project.fastwork.dto.util.LocationDtoUtil.getLocationResponseFr
 @RequiredArgsConstructor
 @Slf4j
 public class LocationController {
-
     private final LocationServiceApi locationService;
-
     @GetMapping("/work/{id_work}")
     public ResponseEntity<?> getLocationByWorkId(@PathVariable("id_work") Long work_id) {
         Location location_by_work;
@@ -33,6 +33,12 @@ public class LocationController {
         }
         LocationResponse location_by_id_response = getLocationResponseFromDomain(location_by_work);
         return ResponseEntity.ok().body(location_by_id_response);
+    }
+
+    @PostMapping("/findbycity")
+    public ResponseEntity<?>findLocationByCity(@RequestBody LocationByCityRequest locationByCityRequest){
+        List<LocationResponse>locationsByCity = locationService.findLocationByCity(locationByCityRequest.getCity()).stream().map(LocationDtoUtil::getLocationResponseFromDomain).toList();
+        return ResponseEntity.ok().body(locationsByCity);
     }
 
 }
