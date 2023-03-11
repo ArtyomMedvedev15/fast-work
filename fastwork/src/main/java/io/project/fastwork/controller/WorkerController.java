13c1e5bde6aebbcf4ccf762ce62d896df2f32d2f
Tim01Bro <arthaos14@gmail.com>
2023-03-12 01:10:48 +0300

    Add work to worker

---
+++ fastwork/src/main/java/io/project/fastwork/controller/WorkerController.java
@@ -0,0 +1,51 @@
+package io.project.fastwork.controller;
+
+import io.project.fastwork.controller.exception.RestWorkAlreadyAddedException;
+import io.project.fastwork.controller.exception.RestWorkNotFoundException;
+import io.project.fastwork.controller.exception.RestWorkerNotFoundException;
+import io.project.fastwork.domains.Users;
+import io.project.fastwork.domains.Work;
+import io.project.fastwork.dto.request.AddWorkWorkerRequest;
+import io.project.fastwork.dto.response.WorkResponse;
+import io.project.fastwork.dto.util.WorkDtoUtil;
+import io.project.fastwork.services.api.UserServiceApi;
+import io.project.fastwork.services.api.WorkServiceApi;
+import io.project.fastwork.services.exception.UserNotFound;
+import io.project.fastwork.services.exception.WorkAlreadyAdded;
+import io.project.fastwork.services.exception.WorkNotFound;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/worker")
+@RequiredArgsConstructor
+@Slf4j
+public class WorkerController {
+
+    private final UserServiceApi userService;
+    private final WorkServiceApi workService;
+    @PostMapping("/addwork")
+    public ResponseEntity<?>addWorkWorker(@RequestBody AddWorkWorkerRequest addWorkWorkerRequest){
+        Work work;
+        Users worker;
+        try {
+            worker = userService.getById(addWorkWorkerRequest.getWorkerId());
+            work = workService.getWorkById(addWorkWorkerRequest.getWorkId());
+            userService.addWorkToWorker(work,worker);
+        } catch (UserNotFound e) {
+            throw new RestWorkerNotFoundException(e.getMessage());
+        } catch (WorkNotFound e) {
+            throw new RestWorkNotFoundException(String.format("Work with id - %s not found",addWorkWorkerRequest.getWorkId()));
+        } catch (WorkAlreadyAdded e) {
+            throw new RestWorkAlreadyAddedException(e.getMessage());
+        }
+        WorkResponse workResponseAdded = WorkDtoUtil.getWorkResponse(work);
+        return ResponseEntity.ok().body(workResponseAdded);
+    }
+
+}
