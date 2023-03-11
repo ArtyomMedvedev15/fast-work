295d81fb50c884d742a37f1b5002b331cfe2aa03
Tim01Bro <arthaos14@gmail.com>
2023-03-11 19:22:31 +0300

    Get all work application by work id

--- fastwork/src/main/java/io/project/fastwork/controller/WorkApplicationController.java
+++ fastwork/src/main/java/io/project/fastwork/controller/WorkApplicationController.java
@@ -1,17 +1,19 @@
 package io.project.fastwork.controller;
 
+import io.project.fastwork.controller.exception.RestWorkNotFoundException;
 import io.project.fastwork.dto.response.WorkApplicationResponse;
 import io.project.fastwork.dto.util.WorkApplicationDtoUtil;
 import io.project.fastwork.services.api.WorkApplicationServiceApi;
+import io.project.fastwork.services.exception.WorkNotFound;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/workapplication")
@@ -22,7 +24,20 @@ public class WorkApplicationController {
 
     @GetMapping("/all")
     public ResponseEntity<?>getAllWorkApplication(){
-        List<WorkApplicationResponse>workApplicationResponses= workApplicationService.findAllWorkApplication().stream().map(WorkApplicationDtoUtil::getWorkApplicationRepsonse).toList();
+        List<WorkApplicationResponse>workApplicationResponses= workApplicationService.findAllWorkApplication().stream()
+                .map(WorkApplicationDtoUtil::getWorkApplicationRepsonse).toList();
         return ResponseEntity.ok().body(workApplicationResponses);
+    }
+
+    @GetMapping("/findbywork/{id_work}")
+    public ResponseEntity<?>getAllWorkAplicationByWorkId(@PathVariable("id_work")Long id_work){
+        List<WorkApplicationResponse>workApplicationResponsesByWorkId;
+        try {
+            workApplicationResponsesByWorkId=workApplicationService.findByWorkId(id_work).stream()
+                    .map(WorkApplicationDtoUtil::getWorkApplicationRepsonse).toList();
+        } catch (WorkNotFound e) {
+            throw new RestWorkNotFoundException(String.format("Work with id - %s not found",id_work));
+        }
+        return ResponseEntity.ok().body(workApplicationResponsesByWorkId);
     }
 }
